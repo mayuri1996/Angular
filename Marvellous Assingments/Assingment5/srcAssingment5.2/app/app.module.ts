c64327830f3babb299eb034bb4dd067a8b546311
mayuri powar <mayuri.powar@ritrl.com>
2019-07-25 14:25:03 +0530

    create two costum comp
    Create Angular application which will create two component named as FirstComp &
    SecondComp.
    FirstComp will display “Hello from First component” on page & SecondComp will display
    “Hello from Second component” on page.
    Render both the components from root component ie App component.

---
+++ Marvellous Assingments/Assingment5/srcAssingment5.2/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import {FormsModule} from '@angular/forms';
+import { FirstCompComponent } from './first-comp/first-comp.component';
+import { SecondCompComponent } from './second-comp/second-comp.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    FirstCompComponent,
+    SecondCompComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
