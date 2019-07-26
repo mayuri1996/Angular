e04534dfccc3dcdc09c3693d91421ac46e86e263
mayuri powar <mayuri.powar@ritrl.com>
2019-07-26 11:29:32 +0530

    event binding
    Create angular application which contains one button. When user press that
    button we have to display some text as “Educating for Better tomorrow” otherwise
    display “Marvellous Infosystems”.

---
+++ Marvellous Assingments/Assingment7/srcAssingment7.3/app/first-comp/first-comp.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-first-comp',
+  templateUrl: './first-comp.component.html',
+  styleUrls: ['./first-comp.component.css']
+})
+export class FirstCompComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+  public fun(){
+    console.log("Educating for Better tomorrow");
+    
+  }
+
+}
